38653ac5cfa498288c6a43b8a14c9ed22f644b24
Sean <sean.nguyen.goldenowl@gmail.com>
2025-05-15 15:02:44 +0700

    feat(fe): add dark mode for page recruitment

--- frontend/app/(root)/recruitment/components/BusinessForm.tsx
+++ frontend/app/(root)/recruitment/components/BusinessForm.tsx
@@ -55,7 +55,7 @@ export default function BusinessForm() {
   }
 
   return (
-    <div className="min-h-screen bg-[#EBF5F4] py-[30px] md:py-[60px]">
+    <div className="min-h-screen dark:bg-[#111827] rounded-lg bg-[#EBF5F4] py-[30px] md:py-[60px]">
       <div className="container mx-auto px-[20px] md:px-[40px]">
         <h3 className=" text-center text-2xl font-bold">
           Đăng ký tài khoản doanh nghiệp
