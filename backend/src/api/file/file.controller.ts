7037b9c8613b011393af09041f7127d230c31cc4
Sean <sean.nguyen.goldenowl@gmail.com>
2025-05-15 16:20:07 +0700

    feat(be): api upload file & delete file

--- backend/src/api/file/file.controller.ts
+++ backend/src/api/file/file.controller.ts
@@ -1,7 +1,83 @@
-import { Controller } from '@nestjs/common';
+import { NotFoundException, Param, UploadedFile } from '@nestjs/common';
 import { FileService } from './file.service';
+import { InjectController, InjectRoute } from '@/decorators';
+import fileRoutes from './file.routes';
+import { FileValidatorPipe } from '@/pipes';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { CloudinaryService } from '../cloudinary/cloudinary.service';
+import { File } from './entities/file.entity';
 
-@Controller('file')
+@InjectController({ name: fileRoutes.index })
 export class FileController {
-  constructor(private readonly fileService: FileService) {}
+  constructor(
+    private readonly fileService: FileService,
+    @InjectDataSource() private readonly dataSource: DataSource,
+    private readonly cloudinaryService: CloudinaryService,
+  ) {}
+
+  @InjectRoute(fileRoutes.upload)
+  async uploadFile(
+    @UploadedFile(
+      new FileValidatorPipe({
+        maxSizeConfig: {
+          size: 1024 * 1024 * 5,
+          errorMessage: 'File size must be less than 5MB',
+        },
+        fileIsRequired: true,
+      }),
+    )
+    file: Express.Multer.File,
+    @Param('folder') folder: string,
+  ) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const { url, key } = await this.cloudinaryService.uploadFile(
+        file,
+        folder,
+      );
+      const newFile = await this.fileService.create(
+        {
+          name: file.originalname,
+          url,
+          key,
+          format: file.mimetype,
+        },
+        queryRunner,
+      );
+      await queryRunner.commitTransaction();
+      return newFile;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  @InjectRoute(fileRoutes.delete)
+  async deleteFile(@Param('id') id: string) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const file = await queryRunner.manager.findOneBy(File, { id });
+      if (!file) {
+        throw new NotFoundException('File not found');
+      }
+      await Promise.all([
+        this.fileService.deleteById(id, queryRunner),
+        this.cloudinaryService.deleteFile(file.key),
+      ]);
+      await queryRunner.commitTransaction();
+      return { message: 'File deleted successfully' };
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
 }
--- backend/src/api/file/file.module.ts
+++ backend/src/api/file/file.module.ts
@@ -3,9 +3,9 @@ import { FileService } from './file.service';
 import { FileController } from './file.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { File } from './entities/file.entity';
-
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([File])],
+  imports: [TypeOrmModule.forFeature([File]), CloudinaryModule],
   controllers: [FileController],
   providers: [FileService],
   exports: [FileService],
