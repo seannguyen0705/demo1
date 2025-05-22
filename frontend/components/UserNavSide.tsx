18bf15389182ad13fca1d5cd6380b7d4c0ba46f3
Sean <sean.nguyen.goldenowl@gmail.com>
2025-05-22 17:56:58 +0700

    fix: hidden nav side on desktop:

--- frontend/components/UserNavSide.tsx
+++ frontend/components/UserNavSide.tsx
@@ -120,7 +120,7 @@ export default function UserNavSide({
   const avatar_url = getUserAvatar(user);
   return (
     <div
-      className={`fixed top-0 right-0 h-full w-64 dark:bg-gray-900 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 ${
+      className={`fixed top-0 lg:hidden right-0 h-full w-64 dark:bg-gray-900 bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50 ${
         openNavSide ? 'translate-x-0' : 'translate-x-full'
       }`}
     >
