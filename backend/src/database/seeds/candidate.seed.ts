4f60a0c05417d394662e83ee7521ee1d4a73fc55
Sean <sean.nguyen.goldenowl@gmail.com>
2025-06-06 22:12:46 +0700

    feat: seed candidate

---
+++ backend/src/database/seeds/candidate.seed.ts
@@ -0,0 +1,23 @@
+// import { DataSource } from 'typeorm';
+// import { Candidate } from '@/api/candidate/entities/candidate.entity';
+// import { faker } from '@faker-js/faker';
+// import { Gender } from '@/common/enums';
+
+// export const seedCandidates = async (dataSource: DataSource, count: number = 10) => {
+//   const candidateRepository = dataSource.getRepository(Candidate);
+
+//   for (const _ of Array.from({ length: count })) {
+//     const candidate = new Candidate();
+//     candidate.email = faker.internet.email();
+//     candidate.password = '@12345678';
+//     candidate.fullName = faker.person.fullName();
+//     candidate.phoneNumber = faker.phone.number();
+//     candidate.gender = Gender.MALE;
+//     candidate.bod = faker.date.birthdate();
+//     candidate.title = faker.lorem.sentence();
+//     candidate.address = faker.location.streetAddress();
+//     candidate.personal_website = faker.internet.url();
+//     candidate.introduction = faker.lorem.paragraph();
+//     await candidateRepository.save(candidate);
+//   }
+// };
--- backend/src/database/seeds/index.ts
+++ backend/src/database/seeds/index.ts
@@ -1,14 +1,14 @@
 import { AppDataSource } from '@/config/ormconfig';
 import { seedProvinces } from './province.seed';
 import { seedSkills } from './skill.seed';
+// import { seedCandidates } from './candidate.seed';
 const seed = async () => {
   await AppDataSource.initialize();
-
   await seedProvinces(AppDataSource);
   await seedSkills(AppDataSource);
-
-  await AppDataSource.destroy();
+  // await seedCandidates(AppDataSource, 20);
 };
+
 seed().catch((error) => {
   console.error(error);
   process.exit(1);
